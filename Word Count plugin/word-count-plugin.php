51a095b58d4e31eb2fc865729341d4e4045280dc
unknown <you@example.com>
2021-10-01 16:48:30 +0300

    fist section form field added

--- Word Count plugin/word-count-plugin.php
+++ Word Count plugin/word-count-plugin.php
@@ -11,15 +11,34 @@ Author URI: https://vahidsediqi.com
 class WordCountAndTimePlugin{
  function __construct(){
     add_action( 'admin_menu', array($this, 'adminPage'));
+    // Below action about the setting funtions that register settings from user
     add_action('admin_init', array($this, 'settings'));
  }
 
 
+//  Setting function will register users setting from dashtboard the first setting is Display Location
  function settings(){
+   add_settings_section( 'wcp_first_section', null, null, 'word-count-settings-page');
+  //  this funtion will add html form to the setting
+  // 1 arg is the plugin name or the group
+  // 2 arg is the field name or label in dashboard
+  // 3 arg is the html template for form fields
+  // 4 arg is the slug
+  // 5 is the section name we make it above
+   add_settings_field('wcp_location', 'Display Location', array($this, 'locationHTML'),'word-count-settings-page', 'wcp_first_section');
     //  1 arg is the name of the group that is belogs to
-
+    // 2 erg is the actual name for the setting 
     register_setting( 'wordcoundplugin', 'wcp_location', array('sanitize_callback' => 'sanitize_text_field' , 'default' => '0')); 
  }
+
+ function locationHTML () { ?>
+   <select name="wcp_location">
+     <option value="x">Select</option>
+     <option value="0">Beginning of post</option>
+     <option value="1">End of post</option>
+   </select>
+ <?php }
+/* This function will display our content under the wordpress dashboard setting */
  function adminPage(){
         /*
     TIPS
@@ -34,8 +53,14 @@ class WordCountAndTimePlugin{
 
 function HTMLtemplate(){ ?>
   <div class="wrap">
-    <h1>Hay You!!!!</h1>
-    <h4>This plugin is made by vahid sediqi</h4>
+    <h1>Word Cound Plugin By: Vahid Sediqi</h1>
+    <form action="options.php" method="POST">
+       <?php
+       settings_fields( 'wordcoundplugin' );
+         do_settings_sections('word-count-settings-page');
+         submit_button();
+       ?>
+    </form>
   </div>
 <?php }
 }
